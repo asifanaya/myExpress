80380b890bf708a129822795dc55017141c29381
INNODASI <mohammad.asif@durr.com>
2021-10-13 16:09:13 +0530

    Changes for flutter web home page

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8de21a3584d614bb57216197322d8b28",
-"/": "8de21a3584d614bb57216197322d8b28",
-"main.dart.js": "ce78318991f999368f1bf18bf066bce0",
+"index.html": "b3d8892077ae7e96a917312cbdb23708",
+"/": "b3d8892077ae7e96a917312cbdb23708",
+"main.dart.js": "3141c3daaf1ffc5419cb5d20e5a3eeea",
 "manifest.json": "57df1e3c84b98dbee1070102b2e86f23",
 "version.json": "96234b88fe514fc4dffc1b8e16fd1356"
 };
